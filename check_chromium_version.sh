6494402467e5aaaea3dcad9a7bdc9128d8d6c5eb
berlogue <carasin.berlogue@mail.ru>
2015-12-12 15:13:39 +0300

    Update to 34.0.2036.25

--- check_chromium_version.sh
+++ check_chromium_version.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
+BUILD_FROM_RPM=0
 mkdir -p ./opera-stable-$1
 pushd ./opera-stable-$1 &> /dev/null
 echo -en "\033[0;35m    Downloading Opera Stable package:\033[0m\n"
-wget -N -q --show-progress ftp://ftp.opera.com/pub/opera/desktop/$1/linux/opera-stable_$1_amd64.deb
-echo -en "\033[0;35m    Opera Stable package hash:\033[0m\n"
-echo -en "\033[0;32m$(md5sum opera-stable_$1_amd64.deb)\033[0m\n"
-ar p opera-stable_$1_amd64.deb data.tar.xz | tar -xJf-
-CHROMIUM_VER=$(strings ./usr/lib/x86_64-linux-gnu/opera/opera | grep Chrome/ | cut --delimiter=/ --fields=2)
+if [ "$BUILD_FROM_RPM" = 1 ]; then
+    wget -N -q --show-progress ftp://ftp.opera.com/pub/opera/desktop/$1/linux/opera-stable_$1_amd64.rpm
+    echo -en "\033[0;35m    Opera Stable RPM package hash:\033[0m\n"
+    echo -en "\033[0;32m$(md5sum opera-stable_$1_amd64.rpm)\033[0m\n"
+    rpm2cpio opera-stable_$1_amd64.rpm | cpio -idV --quiet
+    CHROMIUM_VER=$(strings ./usr/lib64/opera/opera | grep Chrome/ | cut --delimiter=/ --fields=2)
+else
+    wget -N -q --show-progress ftp://ftp.opera.com/pub/opera/desktop/$1/linux/opera-stable_$1_amd64.deb
+    echo -en "\033[0;35m    Opera Stable DEB package hash:\033[0m\n"
+    echo -en "\033[0;32m$(md5sum opera-stable_$1_amd64.deb)\033[0m\n"
+    ar p opera-stable_$1_amd64.deb data.tar.xz | tar -xJf-
+    CHROMIUM_VER=$(strings ./usr/lib/x86_64-linux-gnu/opera/opera | grep Chrome/ | cut --delimiter=/ --fields=2)
+fi
 echo -en "\033[0;35m    Chromium version:\033[0m\n"
 echo -en "\033[0;32m$(echo $CHROMIUM_VER)\033[0m\n"
 echo -en "\033[0;35m    Downloading Chromium sources archive:\033[0m\n"
--- opera-stable-libffmpeg.spec
+++ opera-stable-libffmpeg.spec
@@ -4,8 +4,8 @@
 
 %define chromium_system_libs 0
 
-%define chromium_ver 46.0.2490.86
-%define opera_major_ver 33
+%define chromium_ver 47.0.2526.73
+%define opera_major_ver 34
 %define opera_chan opera-stable
 
 %if 0%{?fedora} >= 21
@@ -16,7 +16,7 @@
 
 Summary:	Additional FFmpeg library for Opera Web browser providing H264 and MP4 support
 Name:		%{opera_chan}-libffmpeg
-Version:	%{opera_major_ver}.0.1990.137
+Version:	%{opera_major_ver}.0.2036.25
 Release:	1%{?dist}
 Epoch:		5
 
@@ -292,6 +292,9 @@ install -m 644 %{_builddir}/%{name}-%{version}/chromium-%{chromium_ver}/out/Rele
 %{_libdir}/%{opera_chan}/lib_extra/libffmpeg.so.*
 
 %changelog
+* Sat Dec 12 2015 carasin berlogue <carasin DOT berlogue AT mail DOT ru> - 5:34.0.2036.25-1
+- Update to 34.0.2036.25
+
 * Mon Nov 30 2015 carasin berlogue <carasin DOT berlogue AT mail DOT ru> - 5:33.0.1990.137-1
 - Update to 33.0.1990.137
 
